73c753cc2d04a0864d8558d133d8219bff77912d
Michael <zwang20@knox.nsw.edu.au>
2019-02-14 09:17:28 +1100

    Setup bug fix

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
         'py2app': {'argv_emulation': True, 'packages': ['pygame']}
     },
 
-    data_files=['sge.py', 'Assets'],
+    data_files=['sge.py', 'Assets', 'map.map'],
     setup_requires=["py2app"],
 )
